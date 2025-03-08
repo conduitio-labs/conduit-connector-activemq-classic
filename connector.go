764afe7b2e834a9b9f0859f02513b65f06b8b941
Guillem <someemail@dot.com>
2025-03-08 20:52:15 +0700

    fix linter

--- connector.go
+++ connector.go
@@ -18,10 +18,12 @@ package activemq
 
 import (
 	_ "embed"
+
 	sdk "github.com/conduitio/conduit-connector-sdk"
 )
 
 // Connector combines all constructors for each plugin in one struct.
+//
 //go:embed connector.yaml
 var specs string
 
